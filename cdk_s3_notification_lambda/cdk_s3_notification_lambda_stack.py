8aed16e7f640a7f16a8794a4637c870c080bcbea
EC2 Default User <ec2-user@ip-172-31-29-201.us-east-2.compute.internal>
2021-01-06 23:26:32 +0000

    commit source

--- cdk_s3_notification_lambda/cdk_s3_notification_lambda_stack.py
+++ cdk_s3_notification_lambda/cdk_s3_notification_lambda_stack.py
@@ -1,5 +1,11 @@
-from aws_cdk import core
+#from aws_cdk import core
 
+from aws_cdk import (
+    aws_lambda as _lambda,
+    aws_s3 as _s3,
+    aws_s3_notifications,
+    core
+)
 
 class CdkS3NotificationLambdaStack(core.Stack):
 
@@ -7,3 +13,18 @@ class CdkS3NotificationLambdaStack(core.Stack):
         super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
+
+        # create lambda function
+        function = _lambda.Function(self, "lambda_function",
+                                    runtime=_lambda.Runtime.PYTHON_3_7,
+                                    handler="lambda-handler.main",
+                                    code=_lambda.InlineCode('boom'))
+                                    
+        # create s3 bucket
+        s3 = _s3.Bucket(self, "s3bucket")
+
+        # create s3 notification for lambda function
+        notification = aws_s3_notifications.LambdaDestination(function)
+
+        # assign notification for the s3 event type (ex: OBJECT_CREATED)
+        s3.add_event_notification(_s3.EventType.OBJECT_CREATED, notification)
